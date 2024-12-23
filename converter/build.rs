57bff4f585c63f1fdada883b2c39c2d250be22dd
Anton Kudryavtsev <dartt0n@ya.ru>
2024-12-23 20:23:38 +0300

    feat(converter): tonic reflections

--- converter/build.rs
+++ converter/build.rs
@@ -1,4 +1,11 @@
+use std::{env, path::PathBuf};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/converter_service.proto")?;
+    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
+
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("converter_service_descriptor.bin"))
+        .compile_protos(&["proto/converter_service.proto"], &["proto"])?;
+
     Ok(())
 }
--- converter/src/main.rs
+++ converter/src/main.rs
@@ -10,6 +10,9 @@ use tracing::info;
 
 pub mod converter_service {
     tonic::include_proto!("converter_service");
+
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("converter_service_descriptor");
 }
 
 use converter_service::converter_service_server::ConverterServiceServer;
@@ -22,12 +25,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         toml::from_str(&fs::read_to_string("config.toml").expect("Failed to read config file"))?;
 
     let addr = format!("{}:{}", config.server.host, config.server.port).parse()?;
-    let converter_service = ConverterServiceImpl::default();
+    let converter_service = ConverterServiceServer::new(ConverterServiceImpl::default());
+    let reflection_service = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(converter_service::FILE_DESCRIPTOR_SET)
+        .build_v1()?;
 
     info!("ConverterService listening on {}", addr);
 
     Server::builder()
-        .add_service(ConverterServiceServer::new(converter_service))
+        .add_service(reflection_service)
+        .add_service(converter_service)
         .serve(addr)
         .await?;
 
