04b35c562dad61df98633f4e40dfbab5a8800c69
Anton Kudryavtsev <dartt0n@ya.ru>
2024-12-30 18:34:31 +0300

    chore(converter): Increase max_width in rustfmt config

--- converter/src/converter/operations.rs
+++ converter/src/converter/operations.rs
@@ -7,20 +7,14 @@ use crate::{
 use tonic::Request;
 
 impl crate::service::ConverterServiceImpl {
-    pub(crate) fn convert_gif_internal(
-        &self,
-        request: Request<GifRequest>,
-    ) -> Result<GifResponse, Box<dyn Error>> {
+    pub(crate) fn convert_gif_internal(&self, request: Request<GifRequest>) -> Result<GifResponse, Box<dyn Error>> {
         Ok(request.into_inner().content)
             .and_then(TGSAnimation::load_from_tgs_bytes)
             .and_then(|tgs| tgs.export_to_gif_bytes())
             .and_then(|content| Ok(GifResponse { content }))
     }
 
-    pub(crate) fn convert_webp_internal(
-        &self,
-        request: Request<WebpRequest>,
-    ) -> Result<WebpResponse, Box<dyn Error>> {
+    pub(crate) fn convert_webp_internal(&self, request: Request<WebpRequest>) -> Result<WebpResponse, Box<dyn Error>> {
         Ok(request.into_inner().content)
             .and_then(TGSAnimation::load_from_tgs_bytes)
             .and_then(|tgs| tgs.export_to_webp_bytes())
--- converter/src/service/converter.rs
+++ converter/src/service/converter.rs
@@ -9,10 +9,7 @@ pub struct ConverterServiceImpl {}
 
 #[tonic::async_trait]
 impl ConverterService for ConverterServiceImpl {
-    async fn convert_to_gif(
-        &self,
-        request: Request<GifRequest>,
-    ) -> Result<Response<GifResponse>, Status> {
+    async fn convert_to_gif(&self, request: Request<GifRequest>) -> Result<Response<GifResponse>, Status> {
         info!("Starting GIF conversion");
 
         match self.convert_gif_internal(request) {
@@ -27,10 +24,7 @@ impl ConverterService for ConverterServiceImpl {
         }
     }
 
-    async fn convert_to_webp(
-        &self,
-        request: Request<WebpRequest>,
-    ) -> Result<Response<WebpResponse>, Status> {
+    async fn convert_to_webp(&self, request: Request<WebpRequest>) -> Result<Response<WebpResponse>, Status> {
         info!("Starting WebP conversion");
 
         match self.convert_webp_internal(request) {
